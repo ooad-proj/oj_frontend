ebbfda2d619f17c346eb294cd1f27b1d088bf477
wzy1935 <13053260125@163.com>
2021-11-09 21:00:28 +0800

    更新样式

--- src/components/MdViewer.vue
+++ src/components/MdViewer.vue
@@ -1,8 +1,8 @@
 <template>
-<div>
+<div class=" tw-overflow-auto">
     <mavon-editor
       :value="value"
-      style="z-index: 0; border: none"
+      style="z-index: 0; border: none; min-height: 50px"
       :boxShadow="false"
       :subfield="false"
       :defaultOpen="'preview'"
